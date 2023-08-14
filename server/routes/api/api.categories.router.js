4c438965493462e026bddbf0c9852335b60fc92c
JerseyPl <Delr1602@gmail.com>
2023-08-14 11:57:24 +0300

    fix page,apiCartRouter

--- server/routes/api/api.categories.router.js
+++ server/routes/api/api.categories.router.js
@@ -1,15 +1,9 @@
 const router = require('express').Router();
-const { Category } = require('../../db/models');
+const { Category, Product, Photo } = require('../../db/models');
 
 router.get('/', async (req, res) => {
   try {
     const categories = await Category.findAll();
-
-    // const products = await Product.findAll({
-    //   include: [{ model: Category }, { model: Photo }],
-    // });
-
-
     res.json(categories);
   } catch (error) {
     res.json({ message: error.message });
@@ -24,6 +18,20 @@ router.get('/:title', async (req, res) => {
       where: { category_id: category.id },
     });
     res.json(products);
+  } catch (error) {
+    res.json({ message: error.message });
+  }
+});
+
+router.get('/:title/:idProd', async (req, res) => {
+  try {
+    const { title, idProd } = req.params;
+    const category = await Category.findOne({ where: { title } });
+    const product = await Product.findOne({
+      include: { model: Photo },
+      where: { category_id: category.id, id: idProd },
+    });
+    res.json(product);
   } catch (error) {
     res.json({ message: error.message });
   }
--- server/routes/index.routes.js
+++ server/routes/index.routes.js
@@ -1,8 +1,10 @@
 const router = require('express').Router();
 const apiProductsRouter = require('./api/api.categories.router');
 const apiAdminProductsRouter = require('./api/api.admin.router');
+const apiCartRouter = require('./api/api.cart.routers');
 
 router.use('/api/categories', apiProductsRouter);
 router.use('/admin/api/products', apiAdminProductsRouter);
+router.use('api/cart', apiCartRouter);
 
 module.exports = router;
