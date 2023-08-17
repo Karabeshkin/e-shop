f9c5ccf37b4beb500654e0bd2c9ec62808f40887
JerseyPl <Delr1602@gmail.com>
2023-08-17 21:00:15 +0300

    bar

--- client/src/components/features/Section/Section.tsx
+++ client/src/components/features/Section/Section.tsx
@@ -6,28 +6,41 @@ function Section(): JSX.Element {
   return (
     <div className="Section">
       <div className="DivspanSection">
-        <img src="./raznoe.png" alt="разное" />
-        <span className="spanSection"> На разные случаи </span>
-      </div>
-      <div className="DivspanSection">
-        <img src="./gotovo.png" alt="напитки " />
+        <img src="./raznoe.png" alt="напитки " />
         <Link to="/categories/Чай" className="spanSection">
           {' '}
           Чай{' '}
         </Link>
       </div>
       <div className="DivspanSection">
-        <img src="./gotovoe.png" alt="готовая еда" />
-        <Link to="/categories/Чай" className="spanSection">
+        <img src="./chips.png" alt="крупы" />
+        <Link to="/categories/Крупы" className="spanSection">
           {' '}
           Крупы{' '}
         </Link>
+      </div>
+      <div className="DivspanSection">
+        <img src="./pasta.png" alt="разное" />
+        <Link to='/categories/Макароны' className="spanSection"> Макароны </Link>
       </div>
       <div className="DivspanSection">
         <img src="./ovoshi.png" alt="овощи" />
         <Link to="/categories/Овощи" className="spanSection">
           {' '}
           Овощи
+        </Link>
+      </div>
+      <div className="DivspanSection">
+        <img src="./cardSection..png" alt="мясо" />
+        <Link to="/categories/Мясо" className="spanSection">
+          Мясо
+        </Link>
+      </div>
+      <div className="DivspanSection">
+      <img src="./gotovo.png" alt="напитки " />
+        <Link to="/categories/Напитки" className="spanSection">
+          {' '}
+          Напитки{' '}
         </Link>
       </div>
       <div className="DivspanSection">
@@ -38,23 +51,17 @@ function Section(): JSX.Element {
         </Link>
       </div>
       <div className="DivspanSection">
-        <img src="./cardSection..png" alt="мясо" />
-        <Link to="/categories/Мясо" className="spanSection">
-          Мясо
-        </Link>
-      </div>
-      <div className="DivspanSection">
-        <img src="./chips.png" alt="чипсы" />
-        <Link to="/categories/Рыба" className="spanSection">
-          {' '}
-          Снеки{' '}
-        </Link>
-      </div>
-      <div className="DivspanSection">
         <img src="./hleb.png" alt="хлеб" />
-        <Link to="/categories/Хлеб" className="spanSection">
+        <Link to="/categories/Хлеб%20и%20выпечка" className="spanSection">
           {' '}
           Хлеб и выпечка{' '}
+        </Link>
+      </div>
+      <div className="DivspanSection">
+        <img src="./fish.png" alt="хлеб" />
+        <Link to="/categories/Рыба" className="spanSection">
+          {' '}
+          Рыба{' '}
         </Link>
       </div>
     </div>
