da7c3a71dfae09d8f0451840e1912219321fa5df
ivo7643 <nahdora001@gmail.com>
2023-08-18 11:38:58 +0300

    Fix Page404

--- client/src/components/features/Page404/NotFoundPage.tsx
+++ client/src/components/features/Page404/NotFoundPage.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import './NotFoundPage.css';
 
 function NotFoundPage(): JSX.Element {
   return (
-    <div>
+    <div className="PageNotFoundContainer">
+      <h1>Такой Страницы не существует</h1>
       <div>
         <img src="/notFound.png" alt="notFound" />
       </div>
